e1e97d89dd616e391b011d9575ccb395e49a214c
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-16 15:55:45 -0500

    test

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
+
 const Contact = () => {
-  const [errMsg, setErrMsg] = useState("");
+	const [errMsg, setErrMsg] = useState("");
 	const [formState, setFormState] = useState({
 		name: "",
 		email: "",
-    message: "",
-    subject:""
+		message: "",
+		subject: "",
 	});
-	const { name, email, message,subject } = formState;
+	const { name, email, message, subject } = formState;
 
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) setErrMsg("Email id is not valid");
-      else setErrMsg("");
-    } else {
-      if (!e.target.value.length)
-        setErrMsg(`${e.target.name} is required`);
-      else setErrMsg("");
-    }
-    
-		if(!errMsg) setFormState({ ...formState, [e.target.name]: e.target.value });
+	const handleChange = (e) => {
+		if (e.target.name === "email") {
+			const isValid = validateEmail(e.target.value);
+			if (!isValid) setErrMsg("Email id is not valid");
+			else setErrMsg("");
+		} else {
+			if (!e.target.value.length) setErrMsg(`${e.target.name} is required`);
+			else setErrMsg("");
+		}
+
+		if (!errMsg)
+			setFormState({ ...formState, [e.target.name]: e.target.value });
 	};
 
-  const handleSubmit = (e) => {
-    	e.preventDefault();
-			console.log(formState);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState);
+	};
 
 	return (
 		<section id="content-area" className="content-area">
@@ -36,11 +37,7 @@ const Contact = () => {
 				<div className="col-lg-4"></div>
 				<div className="col-lg-5 mt-5 mt-lg-0">
 					<h1>Contact Me</h1>
-					<form
-						onSubmit={handleSubmit}
-				
-						
-					>
+					<form onSubmit={handleSubmit}>
 						<div className="row">
 							<div className="col-md-6 form-group">
 								<input
@@ -51,6 +48,7 @@ const Contact = () => {
 									placeholder="Your Name"
 									required
 									onBlur={handleChange}
+									defaultValue={name}
 								/>
 							</div>
 							<div className="col-md-6 form-group mt-3 mt-md-0">
@@ -61,6 +59,7 @@ const Contact = () => {
 									id="email"
 									placeholder="Your Email"
 									required
+									defaultValue={email}
 									onBlur={handleChange}
 								/>
 							</div>
@@ -72,6 +71,7 @@ const Contact = () => {
 								name="subject"
 								id="subject"
 								placeholder="Subject"
+								defaultValue={subject}
 								required
 								onBlur={handleChange}
 							/>
@@ -82,6 +82,7 @@ const Contact = () => {
 								name="message"
 								rows="5"
 								placeholder="Message"
+								defaultValue={message}
 								required
 								onBlur={handleChange}
 							></textarea>
@@ -93,40 +94,7 @@ const Contact = () => {
 					</form>
 				</div>
 				<div className="col-lg-3 mt-5 mt-lg-0"></div>
-				{/* <form id="contact-form" onSubmit={handleSubmit}>
-          <div className="row">
-					 <div className="col-md-6 form-group">
-						<label htmlFor="name">Name</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							defaultValue={name}
-							onBlur={handleChange}
-						></input>
-					</div>
-				 <div className="col-md-6 form-group">
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							defaultValue={email}
-							onBlur={handleChange}
-						></input>
-					</div>
-					 <div className="col-md-6 form-group">
-						<label htmlFor="message">Message</label>
-						<textarea
-							id="message"
-							name="message"
-							defaultValue={message}
-							onBlur={handleChange}
-							rows="5"
-						></textarea>
-					</div>
-          </div>
-				</form> */}
+				{}
 			</div>
 		</section>
 	);
