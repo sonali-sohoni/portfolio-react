8bf0ea3247e3303ddf2b6f39e25797ef80257efe
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-16 18:45:11 -0500

    test

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,6 @@ const Footer = () => {
 	return (
 		<footer id="footer">
 			<div className="container">
-				<div className="copyright">
-					&copy; Copyright{" "}
-					<strong>
-						<span>Sonali Sohoni</span>
-					</strong>
-					&copy;2022 All Rights Reserved
-				</div>
-				<div className="credits"></div>
 				<div className="footer-social-links">
 					<a
 						href="https://github.com/sonali-sohoni"
@@ -19,8 +11,8 @@ const Footer = () => {
 						target="_blank"
 					>
 						<i className="bi bi-github"></i>
-					</a>
-
+					</a>{" "}
+					&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
 					<a
 						href="https://www.linkedin.com/in/sonali-sohoni-3652556/"
 						className="linkedin"
@@ -29,6 +21,14 @@ const Footer = () => {
 						<i className="bi bi-linkedin"></i>
 					</a>
 				</div>
+				<div className="copyright">
+					&copy; Copyright{" "}
+					<strong>
+						<span>Sonali Sohoni</span>
+					</strong>
+					&copy;2022 All Rights Reserved
+				</div>
+				<div className="credits"></div>
 			</div>
 		</footer>
 	);
