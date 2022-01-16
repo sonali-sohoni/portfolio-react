62191dc152c27e050c5a473bb4b6d4d5d6d60cdc
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-16 18:33:24 -0500

    testing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,6 +12,23 @@ const Footer = () => {
 					&copy;2022 All Rights Reserved
 				</div>
 				<div className="credits"></div>
+				<div className="header-social-links">
+					<a
+						href="https://github.com/sonali-sohoni"
+						className="github"
+						target="_blank"
+					>
+						<i className="bi bi-github"></i>
+					</a>
+
+					<a
+						href="https://www.linkedin.com/in/sonali-sohoni-3652556/"
+						className="linkedin"
+						target="_blank"
+					>
+						<i className="bi bi-linkedin"></i>
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
